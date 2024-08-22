195364806cff9921ea5cc9279168ba44cfe4a1de
Sayinko <lauradelyannis@gmail.com>
2024-08-22 20:44:12 +0200

    ajout modale

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,24 +1,59 @@
-async function loginUser() {
-  const loginForm = document.querySelector(".form");
+const form = document.querySelector(".form");
+let user = true;
 
-  loginForm.addEventListener("submit", async function (event) {
-    event.preventDefault();
-    const userMail = document.querySelector("#mail").value;
-    const password = document.querySelector("#pass").value;
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-    try {
-      const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email: userMail, password: password }),
-      });
-      const loginDatas = await response.json();
-      console.log(loginDatas);
-    } catch (error) {
-      console.error(
-        "Il y a un probleme avec la recupération de l'api loginUsers : ",
-        error
-      );
+  const email = document.getElementById("mail").value;
+  const password = document.getElementById("pass").value;
+  const connexionError = document.getElementById("connexion-error");
+  const apiLogin = "http://localhost:5678/api/users/login";
+
+  try {
+    const reponse = await fetch(apiLogin, {
+      method: "post",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!reponse.ok) {
+      alert(`L'email "${email}" ou le mot de passe est incorrect`);
+    } else {
+      const login = await reponse.json();
+      const token = login.token;
+      console.log(token);
+      const cle = login.userId;
+
+      localStorage.setItem("token", token);
+      localStorage.setItem("cle", cle);
+
+      if (email === "sophie.bluel@test.tld" && password === "S0phie") {
+        window.localStorage.setItem("admin", "true");
+      } else {
+        window.localStorage.setItem("admin", "false");
+      }
+
+      window.location.href = "http://127.0.0.1:5500/FrontEnd/index.html";
     }
-  });
-}
+  } catch (error) {
+    console.error("il ya une erreur : " + error);
+  }
+});
+
+/*const header = document.querySelector("header");
+const divEtatConnexion = document.createElement("div");
+const etatConnnexion = document.createElement("p");
+const logoEditionMode = `<i class="fa-regular fa-pen-to-square"></i>`;
+
+divEtatConnexion.classList.add("container-etat-connexion");
+etatConnnexion.classList.add("etat-connexion");
+logoEditionMode.classList.add("logo-connexion");
+
+header.appendChild(divEtatConnexion);
+divEtatConnexion.appendChild(etatConnnexion);
+divEtatConnexion.appendChild(logoEditionMode);
+
+function loginAdmin() {
+  if (user === true) {
+  }
+}*/
--- FrontEnd/test.js
+++ FrontEnd/test.js
@@ -1,129 +1,197 @@
+let travaux = [];
+let categorie = [];
+
+// Récupération des catégories de l'api
+async function categorieApi() {
+  const reponse = await fetch("http://localhost:5678/api/categories");
+  categories = await reponse.json();
+}
+
+// Récupération des travaux de l'api
+async function travauxApi() {
+  const reponse = await fetch("http://localhost:5678/api/works");
+  travaux = await reponse.json();
+}
+
 const gallery = document.querySelector(".gallery");
-const filters = document.createElement("div");
-const allFilters = document.createElement("button");
-const filterApartments = document.querySelector(".filter-appartements");
-const filterHotelsRestaurants = document.querySelector(
-  ".filter-hotelsrestaurants"
-);
 
-let imgWorks;
-let figureImg;
-let figCaption;
-let categoryName;
-let filtersType;
+// Génération de la gallerie d'images
+function generationGallery(travaux) {
+  for (let i = 0; i < travaux.length; i++) {
+    const ficheGallery = document.createElement("article");
+    const imgGallery = document.createElement("img");
+    const legendeImg = document.createElement("figcaption");
 
-filters.appendChild(allFilters);
-gallery.insertAdjacentElement("beforebegin", filters);
-filters.classList.add("filters");
+    gallery.appendChild(ficheGallery);
+    ficheGallery.appendChild(imgGallery);
+    ficheGallery.appendChild(legendeImg);
 
-allFilters.innerHTML = "Tous";
-allFilters.dataset.id = "0";
+    imgGallery.src = travaux[i].imageUrl;
+    legendeImg.innerText = travaux[i].title;
+  }
+}
 
-allFilters.addEventListener("click", () => {
-  const figures = document.querySelectorAll(".gallery figure");
+const filters = document.querySelector(".filters");
+const btnTous = document.createElement("button");
+const btnObjet = document.createElement("button");
+const btnAppartement = document.createElement("button");
+const btnHotelRestaurant = document.createElement("button");
 
-  figures.forEach((figure) => {
-    figure.style.display = "block";
-  });
+btnTous.innerText = "Tous";
+btnObjet.innerHTML = "Objets";
+btnAppartement.innerHTML = "Appartements";
+btnHotelRestaurant.innerHTML = "Hotels & restaurants";
+
+btnTous.classList.add("button_filters");
+btnObjet.classList.add("button_filters");
+btnAppartement.classList.add("button_filters");
+btnHotelRestaurant.classList.add("button_filters");
+
+filters.appendChild(btnTous);
+filters.appendChild(btnObjet);
+filters.appendChild(btnAppartement);
+filters.appendChild(btnHotelRestaurant);
+
+// Filtrer par tous
+btnTous.addEventListener("click", () => {
+  const travauxFiltrees = travaux.filter(
+    (travail) => travail.categoryId === 1 || 2 || 3
+  );
+
+  btnHotelRestaurant.classList.remove("click_color");
+  btnObjet.classList.remove("click_color");
+  btnAppartement.classList.remove("click_color");
+  btnTous.classList.add("click_color");
+
+  gallery.innerHTML = "";
+  generationGallery(travauxFiltrees);
 });
-// Fonction qui à le rôle d'appeler les autres fonctions
-async function init() {
-  try {
-    const categories = await getCategories();
-    displayCategories(categories);
 
-    const works = await getWorks();
-    displayWorks(works);
+// Filtrer objets
+btnObjet.addEventListener("click", () => {
+  const travauxFiltrees = travaux.filter((travail) => travail.categoryId === 1);
 
-    listenerFilters();
-  } catch (error) {
-    console.error("probleme avec la fonction init : ", error);
-  }
+  btnHotelRestaurant.classList.remove("click_color");
+  btnTous.classList.remove("click_color");
+  btnAppartement.classList.remove("click_color");
+  btnObjet.classList.add("click_color");
 
-  // Une fonction qui permet d'ajouter les eventListener sur les filtres
-}
-init();
+  gallery.innerHTML = "";
+  generationGallery(travauxFiltrees);
+});
 
-// Fonction qui récupère les catégories
-async function getCategories() {
-  try {
-    const api = await fetch("http://localhost:5678/api/categories");
-    return await api.json();
-  } catch (error) {
-    console.error("probleme avec la recuperation des categories : ", error);
-  }
-}
+// Filtrer par appartements
+btnAppartement.addEventListener("click", () => {
+  const travauxFiltrees = travaux.filter((travail) => travail.categoryId === 2);
 
-// Fonction qui affiche les catégories
-function displayCategories(categories) {
-  try {
-    categories.forEach((category) => {
-      filtersType = document.createElement("button");
-      filtersType.textContent = category.name;
-      filters.appendChild(filtersType);
+  btnHotelRestaurant.classList.remove("click_color");
+  btnTous.classList.remove("click_color");
+  btnObjet.classList.remove("click_color");
+  btnAppartement.classList.add("click_color");
 
-      filtersType.dataset.id = category.id;
+  gallery.innerHTML = "";
+  generationGallery(travauxFiltrees);
+});
+
+// Filtrer par hotels et restaurants
+btnHotelRestaurant.addEventListener("click", () => {
+  const travauxFiltrees = travaux.filter((travail) => travail.categoryId === 3);
+
+  btnTous.classList.remove("click_color");
+  btnAppartement.classList.remove("click_color");
+  btnObjet.classList.remove("click_color");
+  btnHotelRestaurant.classList.add("click_color");
+
+  gallery.innerHTML = "";
+  generationGallery(travauxFiltrees);
+});
+
+function gererAdmin() {
+  const token = window.localStorage.getItem("token");
+  const adminMode = document.querySelector(".admin-mode");
+  const editMode = document.querySelector(".edit-mode");
+  const editProjet = document.querySelector(".edit-projet");
+  const logout = document.querySelector(".logout");
+  const login = document.querySelector(".login");
+  const titleModifier = document.querySelector(".title-modifier");
+  const modifier = document.querySelector(".title-modifier a");
+  const modale = document.querySelector(".modale");
+  const closeModale = document.querySelector(".close-modale");
+  const galleryModale = document.querySelector(".gallery-modale");
+  const articleImg = document.querySelector(".gallery-modale article");
+
+  // Accès pour l'administrateur
+  if (token) {
+    adminMode.style.display = "flex";
+    logout.style.display = "flex";
+    editMode.style.display = "flex";
+    editProjet.style.display = "flex";
+    modifier.style.display = "flex";
+    login.style.display = "none";
+    filters.style.display = "none";
+    titleModifier.style.margin = "75px";
+
+    // bouton déconnection administrateur
+    logout.addEventListener("click", () => {
+      window.localStorage.removeItem("token");
+      window.location.href = "http://127.0.0.1:5500/FrontEnd/login.html";
     });
-    const allButtons = document.querySelectorAll(".filters button");
-    console.log(allButtons);
 
-    allButtons.forEach((buttons) => {
-      buttons.classList.add("button_filters");
-      buttons.addEventListener("click", () => {
-        buttons.classList.toggle("click_color");
-      });
+    // bouton ouverture de la modale
+    modifier.addEventListener("click", (event) => {
+      event.preventDefault();
+      modale.style.display = "flex";
+      galleryModale.innerHTML = "";
+      generationModale(travaux);
+      gererSuppressionImg(travaux);
     });
-  } catch (error) {
-    console.error("probleme avec l'affichage des catégories : ", error);
-  }
-}
 
-// Fonction qui récupère les travaux
-async function getWorks() {
-  try {
-    const api = await fetch("http://localhost:5678/api/works");
-    return await api.json();
-  } catch (error) {
-    console.error("probleme avec la récupération des travaux : ", error);
-  }
-}
-
-// Fonction qui affiche les travaux
-function displayWorks(works) {
-  try {
-    works.forEach((work) => {
-      imgWorks = document.createElement("img");
-      figureImg = document.createElement("figure");
-      figCaption = document.createElement("figcaption");
-
-      gallery.appendChild(figureImg);
-      figureImg.appendChild(imgWorks);
-      figureImg.appendChild(figCaption);
-
-      imgWorks.src = work.imageUrl;
-      figCaption.textContent = work.title;
-
-      figureImg.dataset.categoryId = work.categoryId;
+    // bouton fermeture de la modale
+    closeModale.addEventListener("click", (event) => {
+      event.preventDefault();
+      modale.style.display = "none";
     });
-  } catch (error) {
-    console.error("probleme avec l'affichage des travaux :", error);
+  } else {
+    adminMode.style.display = "none";
+    logout.style.display = "none";
   }
 }
 
-function listenerFilters() {
-  const buttons = document.querySelectorAll("#portfolio button");
+// création des fiches pour la modale
+function generationModale(travaux) {
+  galleryModale = document.querySelector(".gallery-modale");
+  for (let i = 0; i < travaux.length; i++) {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    const logoPoubelle = document.createElement("i");
+    logoPoubelle.classList.add("bin");
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const figures = document.querySelectorAll(".gallery figure");
-      const categoryId = button.dataset.id;
+    logoPoubelle.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+    img.src = travaux[i].imageUrl;
 
-      figures.forEach((figure) => {
-        figure.style.display = "none";
-        if (categoryId == 0 || figure.dataset.categoryId == categoryId) {
-          figure.style.display = "block";
-        }
-      });
+    galleryModale.appendChild(article);
+    article.appendChild(img);
+    article.appendChild(logoPoubelle);
+  }
+}
+
+function gererSuppressionImg() {
+  const bin = document.querySelectorAll(".bin");
+
+  bin.forEach((bin) => {
+    // COMMENT RELIER LA POUBELLE A LIMAGE SUR LAQUELLE ELLE SE TROUVE POUR POUVOIR LES SUPPRIMER ?
+    bin.addEventListener("click", (event) => {
+      event.preventDefault();
     });
   });
 }
+
+gererAdmin();
+
+async function init() {
+  await categorieApi();
+  await travauxApi();
+  generationGallery(travaux);
+}
+
+init();
